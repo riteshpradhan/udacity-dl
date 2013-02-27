8aa580e33c8273ccf491c3bc4eb72e5a846101d6
Ritesh Pradhan <rp@dev.immunesecurity.com>
2013-02-27 10:11:14 +0545

    Courses name added.

---
+++ udacitydl/courses.py
@@ -0,0 +1,27 @@
+
+COURSES_DICT = {
+
+'cs101': 'Introduction to Computer Science',
+'ph100': 'Introduction to Physics',
+'ST095': 'Udacity Course???',
+'st101': 'Introduction to Statistics',
+'cs215': 'Algorithms: Crunching Social Networks',
+'cs222': 'Differential Equations in Action',
+'cs253': 'Web Development',
+'cs255': 'HTML5 Game Development',
+'cs258': 'Software Testing',
+'cs259': 'Software Debugging',
+'cs262': 'Programming Languages',
+'cs271': 'Introduction to Artificial Intelligence',
+'cs291': 'Interactive Rendering',
+'cs313': 'Introduction to Theoretical Computer Science',
+'ep245': 'How to Build a Startup',
+'cs212': 'Design of Computer Programs',
+'cs344': 'Introduction to Parallel Programming',
+'cs348': 'Functional Hardware Verification',
+'cs373': 'Artificial Intelligence for Robotics',
+'cs387': 'Applied Cryptography',
+'ma006': 'Visualizing Algebra',
+'ma008': 'College Algebra'
+
+}
