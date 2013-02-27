a7d327f82c6d6fe237c4a86079b20832526d0c28
Ritesh Pradhan <rp@dev.immunesecurity.com>
2013-02-27 10:30:04 +0545

    Version upgraded.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="udacity-dl",
-            version="1.1",
+            version="1.1.1",
             description="Download udacity class videos and resources",
             long_description=open("README.md").read(),
             author="Ritesh Pradhan",
