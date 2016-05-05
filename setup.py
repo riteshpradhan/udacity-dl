b75ee73e1c7ead3b60e165220ea4fc6ba8b92266
Ritesh Pradhan <ritesxz@gmail.com>
2016-05-05 11:46:28 -0500

    Changes for 1.2.2 version of udacity-dl.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+# from distutils.core import setup
 
 setup(name="udacity-dl",
-            version="1.2.0",
-            description="Download udacity class videos and resources",
-            long_description=open("README").read(),
-            author="Ritesh Pradhan",
-            author_email="ritesxz@gmail.com",
-            url="https://github.com/riteshpradhan/udacity-dl",
-            packages=["udacitydl"],
-            entry_points = { "console_scripts" : [ "udacity-dl = udacitydl.udacitydl:main"]},
-            install_requires=["mechanize","beautifulsoup4","argparse"],
-           )
+      version="1.2.2",
+      description="Download udacity class videos and resources",
+      long_description=open("README").read(),
+      author="Ritesh Pradhan",
+      author_email="ritesxz@gmail.com",
+      url="https://github.com/riteshpradhan/udacity-dl",
+      download_url="https://pypi.python.org/pypi/udacity-dl",
+      packages=["udacitydl"],
+      entry_points = { "console_scripts" : [ "udacity-dl = udacitydl.udacitydl:main"]},
+      install_requires=["mechanize","beautifulsoup4","argparse"],
+     )
---
+++ udacitydl/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Ritesh Pradhan
+# @Date:   2016-05-03 13:39:08
+# @Last Modified by:   Ritesh Pradhan
+# @Last Modified time: 2016-05-05 11:45:21
